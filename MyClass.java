7edf870bbb9a6f2fd5321ecad6ea990f1d4f71dd
kiruba <kirubakaran1102@gmail.com>
2023-12-16 14:44:41 +0530

    modifed by kiruba

--- MyClass.java
+++ MyClass.java
@@ -2,6 +2,7 @@ class MyClass {
   void add(int x, int y)  //adding two integers 
   {
     System.out.println("Sum of two numbers: " &x+y);
+   
   }
   void  display(){
 }
@@ -21,7 +22,8 @@ class MyClass {
 
 void  add1(int p,int q)
 {
-   
+   System.out.println("Sum of numbers p And q are:" & p+q);
+   System.out.println("Edited by Kiruba");
 }
 
 void mul2(int t,int u)
