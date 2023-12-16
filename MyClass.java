f0f0be6283383749eb0afefe962f8e818b6d1db3
logesh karthick <logeshkarthick67@gmail.com>
2023-12-16 14:18:44 +0530

    commited

--- MyClass.java
+++ MyClass.java
@@ -4,3 +4,15 @@ class MyClass {
     System.out.println("Sum of two numbers: " &x+y);
   }
 }
+   void mul(int x,int y)  //multplying two integers
+   {
+      System.out.println("product of two numbers:" & x*y)
+   }
+
+   void div(int x,int y)
+   {
+      System.out.println("remainder of two numbers :" &X/y)
+   }
+
+
+
